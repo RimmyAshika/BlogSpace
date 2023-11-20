9c250ceda7c847efbbbb14e1671d7a58b7679f87
Anik Adhikari <developwithanik@gmail.com>
2023-11-20 20:53:40 +0530

    complete: (with error)

---
+++ src/pages/Signup.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Login as signupComponent}  from '../components'
+
+function Signup() {
+    return (
+        <div className="py-8">
+            <signupComponent />
+        </div>
+    );
+}
+
+export default Signup;
