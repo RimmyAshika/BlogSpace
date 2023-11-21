fcc4dc604ad2b70ce8add756ad7ebf1c9af63788
Anik Adhikari <developwithanik@gmail.com>
2023-11-21 12:01:44 +0530

    added: logo & title

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -1,9 +1,12 @@
-import React from 'react'
+/* eslint-disable react/prop-types */
+import React from 'react';
 
-function Logo({width = '100px'}) {
-  return (
-    <div>Logo</div>
-  )
+function Logo({ width = '100px' }) {
+    return (
+        <div>
+            <img src="../../public/logo.png" alt="Logo" width={width} />
+        </div>
+    );
 }
 
-export default Logo
+export default Logo;
