3b53dd28213cb8394579dd75487d2a0dcb864e5c
Anik Adhikari <developwithanik@gmail.com>
2023-11-30 19:06:43 +0530

    fixed: some minor bugs

--- src/components/PostCard.jsx
+++ src/components/PostCard.jsx
@@ -5,14 +5,7 @@ import appwriteService from '../appwrite/config';
 
 function PostCard(post) {
     const { $id, title, featuredImage, content } = post;
-    // Function to extract the first two lines of content
-    const extractFirstTwoLines = (content) => {
-        const lines = content.split('\n');
-        return lines.slice(0, 2).join('\n');
-    };
 
-    // Render only the first two lines of content
-    const truncatedContent = extractFirstTwoLines(content);
     return (
         <div className="p-4 bg-white bg-opacity-30 backdrop-filter backdrop-blur-lg md:w-[1200px] w-[350px] mx-auto  2xl:w-[1250px] rounded-lg">
             <div className="flex flex-col md:flex-row items-center gap-4">
@@ -22,13 +15,13 @@ function PostCard(post) {
                     className="w-[270px] rounded-l-lg aspect-video object-cover object-top"
                 />
                 <div className="flex flex-col md:items-start items-center gap-5">
-                    <h1 className="text-4xl font-bold">{title}</h1>
-                    <p className="text-xl line-clamp-2">
-                        {parse(truncatedContent)}
-                    </p>
+                    <h1 className="text-4xl font-bold text-start line-clamp-1">
+                        {title}
+                    </h1>
+                    <p className="text-xl line-clamp-1">{parse(content)}</p>
                     <Link
                         to={`/post/${$id}`}
-                        class="rounded-md border-2 border-black bg-black px-3 py-2 text-white transition-all duration-500 hover:bg-inherit hover:text-black"
+                        className="rounded-md border-2 border-black bg-black px-3 py-2 text-white transition-all duration-500 hover:bg-inherit hover:text-black"
                     >
                         Read more
                     </Link>
